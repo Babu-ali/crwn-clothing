fb3dc0f3bb2ed03edb390828639cf30db40afaa6
babua <babua@maveric-systems.com>
2021-06-11 16:17:10 +0530

    corrected currentUser typo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth } from './firebase/firebase.utils';
 
+
 import { Switch, Route } from 'react-router-dom';
 
 class  App extends React.Component {
@@ -14,7 +15,7 @@ class  App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null
@@ -34,7 +35,7 @@ class  App extends React.Component {
   render(){
     return (
       <div>
-        <Header />
+        <Header currentUser={ this.state.currentUser } />
         <Switch>
             <Route exact={true} path='/' component={HomePage} />
             <Route exact={true} path='/shop' component={ ShopPage } />
