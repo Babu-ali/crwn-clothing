97e29fb92821b92c62bd24e0402d3fbe3751077e
babua <babua@maveric-systems.com>
2021-06-18 00:55:14 +0530

    changed cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
-import {ReactComponent as Logo} from '../../assests/crown.svg';
+//import {ReactComponent as Logo} from '../../assests/crown.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => (
     <div className='cart-icon'>
-        <Logo className='shopping-icon'/>
+        <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
 );
