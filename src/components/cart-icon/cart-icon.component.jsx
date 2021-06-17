82a92c6f375056c42471145d6f3fa2bfda98bd8e
babua <babua@maveric-systems.com>
2021-06-17 18:08:23 +0530

    correct typo in cart-component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = () => {
+const CartIcon = () => (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
-
     </div>
-}
+);
+    
 
 export default CartIcon;
