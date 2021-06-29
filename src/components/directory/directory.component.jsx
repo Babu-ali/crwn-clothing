0927c8d8b35b09ae837f8fd087e569fcd4db8837
babua <babua@maveric-systems.com>
2021-06-29 16:23:16 +0530

    modified states of hompage,shop to redux pattern. Built separate collection overview component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../directory/directory.selectors';
+
+
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
@@ -8,58 +13,18 @@ import sneakers from '../../sneakers.jpeg'
 import women from '../../women.jpeg'
 import men from '../../men.jpeg'
 
-class Directory extends React.Component {
-    constructor(){
-        super();
-
-        this.state = {
-            sections : [
-                {
-                  title: 'hats',
-                  imageUrl: hat, //'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: jackets,//'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: sneakers,//'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: women,//'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: men,//'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-              ]
-        }
+const Directory = ( { sections }) => (
+    <div className='directory-menu'>
+    {
+        sections.map(({id, ...otherSectionProps})=>(
+        <MenuItem key={id} {...otherSectionProps} />
+        ))
     }
+  </div>
+);
 
-    render(){
-        return(
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({id, ...otherSectionProps})=>(
-                    <MenuItem key={id} {...otherSectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
-
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+  
+export default connect(mapStateToProps)(Directory);
